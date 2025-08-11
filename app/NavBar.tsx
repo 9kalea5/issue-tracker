63380afd754a80dd6232dfe27890188c93095ba5
kaleab <alebachewkaleab@gmail.com>
2025-08-11 22:49:36 +0300

    Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
+import { IoBugSharp } from "react-icons/io5";
 
 const NavBar = () => {
+    const links = [
+        {label: 'Dashbord', href:'/'},
+        {label: 'Issues', href:'/issues'},
+    ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/">Logo</Link>
+        <Link href="/"><IoBugSharp /></Link>
         <ul className='flex space-x-6'>
-            <li><Link href="/">Dashborad</Link></li>
-            <li><Link href="/issues">Issues</Link></li>
+            {links.map(link => 
+                <Link key={link.href} 
+                className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
