eda8db6e9943c29aab2ca0d75c0c39a048874fc6
kaleab <alebachewkaleab@gmail.com>
2025-08-17 23:41:50 +0300

    Loading Graphics

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -22,6 +22,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   });
   const [error, setError] = useState('')
+  const [isSubmitting, setSubmitting] = useState(false)
 
   return (
     <div className='max-w-xl'>
@@ -32,9 +33,11 @@ const NewIssuePage = () => {
         className='space-y-3'
         onSubmit={handleSubmit(async(data) => {
           try {
+            setSubmitting(true)
             await axios.post('/api/issues', data)
             router.push('/issues')
           } catch (error) {
+            setSubmitting(false)
             setError('An unexpected error')
           }
         })}
@@ -61,7 +64,7 @@ const NewIssuePage = () => {
         <ErrorMessage>
           {errors.description?.message}
         </ErrorMessage>
-        <Button>Submit New Issue<Spinner /></Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   );
