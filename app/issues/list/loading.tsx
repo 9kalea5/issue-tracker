3e95b6db5f6726a9b4ab0f89d2d542ab243d0636
kaleab <alebachewkaleab@gmail.com>
2025-08-25 13:19:07 +0300

    Reorganize

--- app/issues/list/loading.tsx
+++ app/issues/list/loading.tsx
@@ -1,7 +1,4 @@
-import { Table } from '@radix-ui/themes'
-import React from 'react'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import Skeleton from 'react-loading-skeleton'
+import { Skeleton, Table } from '@radix-ui/themes'
 import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActions'
 
